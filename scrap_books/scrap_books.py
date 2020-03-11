b562e7b44df94133271cdb7a18c8f3ff80cebc82
Darshan Kathiriya <darshan@pop-os.localdomain>
2020-03-11 20:46:11 -0300

    Changes in Script

--- scrap_books/scrap_books.py
+++ scrap_books/scrap_books.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[44]:
 
 
 # !conda install -c anaconda beautifulsoup4 requests pymongo -y
@@ -12,40 +12,48 @@ import re
 from pymongo import MongoClient
 import time
 import datetime
+import os
 
 
-# In[2]:
+# In[12]:
 
 
 response = requests.get("http://www.gutenberg.org/wiki/Gutenberg:Offline_Catalogs")
 
 
-# In[3]:
+# In[13]:
 
 
 html_soup = bs(response.text, 'html.parser')
 
 
-# In[6]:
+# In[149]:
 
 
 def process_txt(text):
     book_list=[]
     text = text.encode('utf-8').decode("ascii","ignore")
     # pattern fails to match every deatils for very long titles
-    for match in re.findall(r"(^(?:[\w,.-]+\s)+)\s+(\d{5})\s+((?:\[.*?\n*.*?\])(?:\s)*)*",text, re.MULTILINE):
+#     for match in re.findall(r"(^(?:[\w,.-\':]+\s)+)\s+(\d{5})\s+((?:\[.*?\n*.*?\])(?:\s)*)*",text, re.MULTILINE):
+    for match in re.findall(r"^([\w.,-:;&()\/' ]+)  *\d+ *(\n[\w.,-:' \[\]]+)*$",text, re.MULTILINE):
+#         print(match)
+#         print(', by' in match)
         # ignore non english books with [Language: *]
-        if not re.match("\[Language:",match[-1].strip()):
+        if (not re.match("\[Language:",match[-1].strip())) and (', by' in match[0]):
+#             print(match)
             book_details = [x.strip() for x in match[0].split(', by')]
+#             print(book_details)
             # ignore books with no author name
             if len(book_details) > 1 and len(book_details) < 3:
 #                 title, author = book_details
                 book_list.append(book_details)
     return book_list
-mongoConnectionString="mongodb://csciDarshan:csciDarshan123@docdb-2020-03-11-00-58-01.cluster-chnyvyrv3c0z.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 
 
-def insert_books(file, book_list, start_time, end_time):
+mongoConnectionString="mongodb://csciDarshan:csciDarshan123@docdb-2020-03-11-01-33-36.cluster-chnyvyrv3c0z.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+
+
+def insert_books(file_name, book_list, start_time, end_time):
     client = MongoClient(mongoConnectionString)
     booksdb = client["gu_books"]
     bookscol = booksdb["books"]
@@ -56,23 +64,17 @@ def insert_books(file, book_list, start_time, end_time):
 #     print(bookscol.inserted_ids)
 
 
-# In[8]:
+# In[150]:
 
 
-#mw-content-text > div > ul:nth-child(12)
-for li in html_soup.select("#mw-content-text > div > ul:nth-child(12) li"):
-    file_name = li.a["href"].split("/")[-1]
-    print(file_name)
-    start_time = datetime.datetime.now()
-    txt_file = requests.get("http:"+str(li.a["href"])).text
-    book_list = process_txt(txt_file)
-    end_time = datetime.datetime.now()
-    insert_books(file_name, book_list, start_time, end_time)
-    time.sleep(300)
-
-
-# In[ ]:
-
-
+books_list=[]
+for root,_,files in os.walk("data/"):
+    for f in files:
+        start_time = datetime.datetime.now()
+        with open(os.path.join(root, f),'r') as fl:
+            books_list = process_txt(fl.read())
+        end_time = datetime.datetime.now()
+        insert_books(f, books_list, start_time, end_time)
+#         time.sleep(300)
 
 

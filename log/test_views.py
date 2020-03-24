675b0201f875655761b44c223ef59e2633d77600
Darshan Kathiriya <darshan@pop-os.localdomain>
2020-03-24 14:58:44 -0300

    add tests to all services

---
+++ log/test_views.py
@@ -0,0 +1,43 @@
+from app import app
+import os
+import unittest
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+
+###############
+#### tests ####
+###############
+
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_log(self):
+        response = self.app.get(
+            '/get_log')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_log(self):
+        response = self.app.post(
+            '/log',data=dict(req_time="12-12-2020",keyword="Test_Case"))
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ notes/test_views.py
@@ -0,0 +1,43 @@
+from app import app
+import os
+import unittest
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+
+###############
+#### tests ####
+###############
+
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_notes(self):
+        response = self.app.get(
+            '/notes', query_string=dict(keyword='The Apostolic Tradition of Hippolytus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_note(self):
+        response = self.app.post(
+            '/add_note', data=dict(keyword='Test_Case',note="Test_Case_Note"))
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
